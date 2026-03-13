edd0b7b00f6d7d1b705d317697695632af373bae
Fernando Quintero <ferdanielq@outlook.com>
2026-03-12 23:37:37 -0400

    update nvim a bit

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,19 +1,4 @@
-require "config.lazy"
-require "user.treesitter"
-require "user.format"
+require_all = (require "helpers").require_all
 
-vim.api.nvim_create_user_command('SyntaxHere', function()
-	local pos = vim.api.nvim_win_get_cursor(0)
-	local line, col = pos[1], pos[2] + 1
-	print 'Regex (legacy) Syntax Highlights'
-	print '--------------------------------'
-	print(' effective: ' .. vim.fn.synIDattr(vim.fn.synID(line, col, true), 'name'))
-	for _, synId in ipairs(vim.fn.synstack(line, col)) do
-		local synGroupId = vim.fn.synIDtrans(synId)
-		print(' ' .. vim.fn.synIDattr(synId, 'name') .. ' -> ' .. vim.fn.synIDattr(synGroupId, 'name'))
-	end
-	print ' '
-	print 'Tree-sitter Syntax Highlights'
-	print '--------------------------------'
-	vim.print(vim.treesitter.get_captures_at_cursor(0))
-end, {})
+require_all "user"
+
