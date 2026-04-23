262d33a117bd95b905b5a967d5589ceb4bf1c105
Fernando D Quintero (agent) <ferdanielq@outlook.com>
2026-04-22 21:44:12 -0400

    pwsh: add helper aliases (ll, la, g) and which; enable MenuComplete

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -5,3 +5,23 @@ if (Test-Path $ompConfig) {
 }
 
 Import-Module git-completion
+
+if (Get-Command Set-PSReadLineKeyHandler -ErrorAction SilentlyContinue) {
+    Set-PSReadLineKeyHandler -Chord Tab -Function MenuComplete
+}
+
+Set-Alias g git
+
+function ll {
+    Get-ChildItem @args
+}
+
+function la {
+    Get-ChildItem -Force @args
+}
+
+function which {
+    param([Parameter(Mandatory)] [string] $Name)
+    Get-Command $Name -All |
+        Select-Object CommandType, Name, Source
+}
